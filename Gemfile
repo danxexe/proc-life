1ee3519b36c49ee05726a1eea712e6cd8f296ada
DanX <danx.exe@gmail.com>
2012-10-11 14:14:47 -0300

    sprockets

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'http://rubygems.org'
 
 gem 'rack-server-pages'
+gem 'sprockets'
 gem 'slim'
 gem 'sass'
 gem 'coffee-script'
---
+++ assets/javascripts/application.js
@@ -0,0 +1,16 @@
+//= require rAF
+//= require jquery
+//= require fabric
+//= require fabric.hover
+//= require javascript-vehicles/Class
+//= require javascript-vehicles/Vector2D
+//= require javascript-vehicles/Vehicle
+//= require javascript-vehicles/SteeredVehicle
+
+//= require grid
+//= require creature_data
+//= require creature_frame
+//= require creature
+//= require main
+//= require riffwave
+//= require worker
